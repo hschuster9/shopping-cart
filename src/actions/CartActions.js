42003392413dec4507eb58a6275b21722cd93a30
Hannah Schuster <hschuster9@gmail.com>
2017-03-30 11:20:16 -0400

    completed

---
+++ src/actions/CartActions.js
@@ -0,0 +1,9 @@
+export const addToCart = (item) => {
+
+  console.log(`ACTION: adding ${item} to cart`)
+  return {
+
+    type: 'ADD_ITEM',
+    item 
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
+
+import App from './components/App'
+import Store from './Store'
+
+
+const StoreInstance = Store()
 
 ReactDOM.render(
-  <App />,
+  <Provider store={StoreInstance}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
